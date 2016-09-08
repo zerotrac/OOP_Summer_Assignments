21c8f969db886be6c121dc31db7cc4cbfb453284
zerotrac <734688610@qq.com>
2016-09-08 21:43:32 +0800

    设置工作路径，这样在mac上就可以发布了

--- OOP_Term_Project_PVZ/OOP_Term_Project_PVZ/main.cpp
+++ OOP_Term_Project_PVZ/OOP_Term_Project_PVZ/main.cpp
@@ -1,12 +1,13 @@
 #include <QApplication>
 #include <ctime>
+#include <QDir>
 #include "logic.h"
 
 int main(int argc, char *argv[])
 {
     std::srand(time(nullptr));
-
     QApplication a(argc, argv);
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
 
     Logic* logic = new Logic();
     logic->execute();
